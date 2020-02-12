d9dd085fd382f436a25f4aa0f6101c52ecfe2c00
lindacoco <lindacoco0204@gmail.com>
2020-02-12 17:01:37 +0900

    사진넣기

--- src/main/java/gradle_jdbc_study/ui/content/EmployeePanel.java
+++ src/main/java/gradle_jdbc_study/ui/content/EmployeePanel.java
@@ -2,10 +2,13 @@ package gradle_jdbc_study.ui.content;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.util.Date;
 import java.util.List;
 import java.util.Vector;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,10 +18,24 @@ import javax.swing.JTextField;
 import gradle_jdbc_study.dto.Department;
 import gradle_jdbc_study.dto.Employee;
 import gradle_jdbc_study.dto.Title;
+import gradle_jdbc_study.ui.Listener.MyDocumentListener;
 
 import javax.swing.SwingConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import com.toedter.calendar.JDateChooser;
+
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+import javax.swing.BoxLayout;
+import java.awt.Dimension;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class EmployeePanel extends AbsItemPanel<Employee> implements ItemListener  {
@@ -27,12 +44,29 @@ public class EmployeePanel extends AbsItemPanel<Employee> implements ItemListene
 	private JComboBox<Department> cmbDept;
 	private JComboBox<Employee> cmbManager;
 	private JComboBox<Title> cmbTitle;
+	private JPasswordField pfPasswd;
+	private JPasswordField pfPasswd2;
+	private JLabel lblPasswdEqual;
+	private Dimension picDimension=new Dimension(100, 150);
+	private JLabel lblPic;
 
 	public EmployeePanel() {
 		setLayout(new BorderLayout(0, 0));
 		
 		JPanel pWest = new JPanel();
 		add(pWest, BorderLayout.WEST);
+		pWest.setLayout(new BoxLayout(pWest, BoxLayout.Y_AXIS));
+		
+		lblPic = new JLabel();
+		lblPic.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPic.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		lblPic.setPreferredSize(picDimension);
+		lblPic.setSize(picDimension);
+		setPic(getClass().getClassLoader().getResource("no-image.png").getPath());
+		pWest.add(lblPic);
+		
+		JButton btnPic = new JButton("증명사진");
+		pWest.add(btnPic);
 		
 		JPanel pCenter = new JPanel();
 		add(pCenter, BorderLayout.CENTER);
@@ -76,6 +110,88 @@ public class EmployeePanel extends AbsItemPanel<Employee> implements ItemListene
 		
 		cmbTitle = new JComboBox<>();
 		pCenter.add(cmbTitle);
+		
+		JLabel lblHireDate = new JLabel("입사일");
+		lblHireDate.setHorizontalAlignment(SwingConstants.RIGHT);
+		pCenter.add(lblHireDate);
+		
+		JDateChooser tfHireDate = new JDateChooser(new Date(), "yyyy-MM-dd hh:mm");
+		pCenter.add(tfHireDate);
+		
+		JLabel lblSalary = new JLabel("급여");
+		lblSalary.setHorizontalAlignment(SwingConstants.RIGHT);
+		pCenter.add(lblSalary);
+		
+		JSpinner spSalary = new JSpinner();
+		spSalary.setModel(new SpinnerNumberModel(1500000, 1000000, 5500000, 100000));
+		pCenter.add(spSalary);
+		
+		JLabel lblPasswd = new JLabel("비밀번호");
+		lblPasswd.setHorizontalAlignment(SwingConstants.RIGHT);
+		pCenter.add(lblPasswd);
+		
+		pfPasswd = new JPasswordField();
+		pfPasswd.getDocument().addDocumentListener(new MyDocumentListener() {
+			
+			@Override
+			public void msg() {
+				String pw1 = new String(pfPasswd.getPassword());
+				String pw2 = new String(pfPasswd2.getPassword());
+				if(pw1.length() ==0 || pw2.length() == 0) {
+					lblPasswdEqual.setText("");
+				}else if(pw1.contentEquals(pw2)) {
+					lblPasswdEqual.setText("비밀번호일치");
+				}else {
+					lblPasswdEqual.setText("불일치");
+				}
+			}
+		});
+		pfPasswd.setText("");
+		pCenter.add(pfPasswd);
+		
+		JLabel lblPasswd2 = new JLabel("비밀번호확인");
+		lblPasswd2.setHorizontalAlignment(SwingConstants.RIGHT);
+		pCenter.add(lblPasswd2);
+		
+		pfPasswd2 = new JPasswordField();
+		pfPasswd2.getDocument().addDocumentListener(new MyDocumentListener() {
+			
+			@Override
+			public void msg() {
+				String pw1 = new String(pfPasswd.getPassword());
+				String pw2 = new String(pfPasswd2.getPassword());
+				if(pw1.length() ==0 || pw2.length() == 0) {
+					lblPasswdEqual.setText("");
+				}else if(pw1.contentEquals(pw2)) {
+					lblPasswdEqual.setText("비밀번호일치");
+				}else {
+					lblPasswdEqual.setText("불일치");
+				}
+			}
+		});
+		
+		
+		
+		
+		
+		DocumentListener docListener = new MyDocumentListener() {
+			
+			@Override
+			public void msg() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		pfPasswd2.setText("");
+		pCenter.add(pfPasswd2);
+		
+		JPanel panel = new JPanel();
+		pCenter.add(panel);
+		
+		lblPasswdEqual = new JLabel("불일치");
+		lblPasswdEqual.setFont(new Font("Gulim", Font.PLAIN, 12));
+		lblPasswdEqual.setHorizontalAlignment(SwingConstants.CENTER);
+		pCenter.add(lblPasswdEqual);
 
 	}
 
@@ -142,7 +258,9 @@ public class EmployeePanel extends AbsItemPanel<Employee> implements ItemListene
 	}
 
 	
-
+	private void setPic(String imgPath) {
+		lblPic.setIcon(new ImageIcon(new ImageIcon(imgPath).getImage().getScaledInstance((int)picDimension.getWidth(),(int)picDimension.getHeight(), Image.SCALE_DEFAULT)));
+	}
 	
 
 }
